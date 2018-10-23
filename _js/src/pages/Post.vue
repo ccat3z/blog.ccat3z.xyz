eac5921aeacbd0bc89798fe1d79b38eadf1890c3
ccat3z <c0ldcat3z@gmail.com>
2018-10-23 13:12:33 +0800

    change default post image size

--- _js/src/pages/Post.vue
+++ _js/src/pages/Post.vue
@@ -42,7 +42,7 @@ var Trianglify = require('trianglify')
 
 export function getPostInfo (e) {
   let id = hash($('a.post-title', e).attr('href'))
-  let image = $('img.post-image', e).attr('src') || Trianglify({ width: 512, height: 512, seed: id }).png()
+  let image = $('img.post-image', e).attr('src') || Trianglify({ width: 512, height: 256, seed: id }).png()
   console.log($('img.post-image', e))
 
   return {

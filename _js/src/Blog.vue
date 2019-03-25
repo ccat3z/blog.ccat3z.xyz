20ea8c35522347c2d39e6ecbecc80db90b23bb97
ccat3z <c0ldcat3z@gmail.com>
2019-03-25 23:41:46 +0800

    Revert "support icp"
    This reverts commit 47f986953ec0cf8b988509b214fdb932aa5826db.

--- _js/src/Blog.vue
+++ _js/src/Blog.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <!-- <background /> -->
-    <md-card-transition-content class="blog-main-ui">
+    <md-card-transition-content>
       <home v-if="pageType === 'home'">
       </home>
       <post v-else-if="pageType === 'post'" />
@@ -10,7 +10,6 @@
       <message-base v-else color="amber" icon="format_paint" message="Coming soon"/>
     </md-card-transition-content>
     <Nav :show="pageType !== 'home' || isLoading" :is-processing="isLoading"/>
-    <ICP :show="pageType === 'home'" />
     <v-snackbar v-model="accent" bottom :timeout="3000" color="red accent-2">{{ message }}</v-snackbar>
   </v-app>
 </template>
@@ -32,7 +31,6 @@ import MessageBase from 'pages/MessageBase.vue'
 import Nav from 'components/layout/Nav.vue'
 import Background from 'components/layout/Background.vue'
 import MdCardTransitionContent from 'components/layout/MdCardTransitionContent.vue'
-import ICP from 'components/ICP.vue'
 
 import 'app/style/hide-orig-data.css'
 
@@ -105,7 +103,7 @@ export default {
   },
   store,
   components: {
-    Nav, Home, Post, Posts, Message, MessageBase, MdCardTransitionContent, Background, ICP
+    Nav, Home, Post, Posts, Message, MessageBase, MdCardTransitionContent, Background
   },
   router
 }
--- _js/src/store/blog.js
+++ _js/src/store/blog.js
@@ -62,10 +62,6 @@ function getNav (data) {
   }).toArray()
 }
 
-function getICP (data) {
-  return $('#blog-data-icp').text()
-}
-
 const state = {
   _blogData: getBlogData()
 }
@@ -76,8 +72,7 @@ const getters = {
   content: (state, getters) => getters._blogContent.content,
   pagination: (state) => getPagination(state._blogData),
   nav: (state) => getNav(state._blogData),
-  rootPath: (state, getters) => getters.pagination.rootPath,
-  icp: (state) => getICP(state._blogData)
+  rootPath: (state, getters) => getters.pagination.rootPath
 }
 
 const mutations = {

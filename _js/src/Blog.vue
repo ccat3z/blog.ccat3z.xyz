6003fb234f9278fe7b1e1756cca702c4e0b11f65
ccat3z <c0ldcat3z@gmail.com>
2019-03-30 19:05:15 +0800

    refactor(js/blog): split router

--- _js/src/Blog.vue
+++ _js/src/Blog.vue
@@ -16,7 +16,6 @@
 
 <script>
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 
 import 'material-design-icons/iconfont/material-icons.css'
 import 'vuetify/dist/vuetify.min.css'
@@ -34,62 +33,10 @@ import MdCardTransitionContent from 'components/layout/MdCardTransitionContent.v
 
 import 'app/style/hide-orig-data.css'
 
-import { log } from 'app/utils/common'
 import store from 'app/store'
-import axios, { CancelToken } from 'axios'
+import router from './router'
 
-Vue.use(VueRouter)
 Vue.use(Vuetify)
-
-var router = new VueRouter({
-  mode: 'history',
-  routes: [
-    { path: '*' }
-  ]
-})
-
-let cancelLoad
-
-router.beforeEach(async function loadBlogPage (to, from, next) {
-  var blog = router.app
-  if (blog.isLoading === undefined) {
-    next()
-    return
-  }
-
-  if (!blog.isLoading) {
-    // ready to router
-    log.d('router', 'loading ' + to.fullPath)
-    blog.isLoading = true
-    try {
-      let resp = await axios.get(to.path, {
-        cancelToken: new CancelToken((c) => (cancelLoad = () => {
-          c()
-          blog.isLoading = false
-        }))
-      })
-
-      blog.$store.commit('blog/reloadBlogData', resp.data)
-
-      log.d('router', 'loaded ' + to.fullPath)
-      next()
-    } catch (e) {
-      if (axios.isCancel(e)) {
-        log.d('router', 'canceled ' + to.fullPath)
-      } else {
-        blog.showAccentMessage(e.response ? e.response.status : e.message)
-      }
-
-      log.e('router', 'fail to load ' + to.fullPath)
-    } finally {
-      blog.isLoading = false
-    }
-  } else {
-    // something is loading
-    cancelLoad()
-    loadBlogPage(to, from, next)
-  }
-})
 
 export default {
   data: () => ({
@@ -116,5 +63,4 @@ export default {
 </script>
 
 <style>
-/* html { overflow-y: auto; } */
 </style>

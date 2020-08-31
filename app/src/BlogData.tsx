4829f9fdb7b24c2f4aff26baff137db39c16e56d
ccat3z <c0ldcat3z@gmail.com>
2020-08-31 22:38:18 +0800

    add useDlogDataLoadingState

--- app/src/BlogData.tsx
+++ app/src/BlogData.tsx
@@ -5,16 +5,31 @@ import { fetchBlogData, usePromise, BlogData } from './utils'
 const BlogContext = React.createContext<BlogData>({ type: 'splash' })
 export function useBlogData() { return useContext(BlogContext) }
 
+const defaultLoadingState = {
+  state: 'resolved' as 'pending' | 'rejected' | 'resolved',
+  retry: () => {}
+}
+const LoadingStateContext = React.createContext(defaultLoadingState)
+export function useBlogDataLoadingState() { return useContext(LoadingStateContext) }
+
 export default function BlogDataContext(props: { children: React.ReactNode }) {
   const { push } = useHistory(); (global as any).router = push
   const { pathname } = useLocation()
   const [initPath] = useState(pathname)
-  const { data: _blogData } = usePromise(() => fetchBlogData(pathname === initPath ? undefined : pathname), [pathname])
+  const { data: _blogData, state, retry } = usePromise(() => fetchBlogData(pathname === initPath ? undefined : pathname), [pathname])
   const blogData = useMemo(() => _blogData || { type: 'splash' }, [_blogData])
+  const loadingState = useMemo(() => {
+    return {
+      state,
+      retry
+    }
+  }, [state, retry])
 
   return (
     <BlogContext.Provider value={blogData}>
-      {props.children}
+      <LoadingStateContext.Provider value={loadingState}>
+        {props.children}
+      </LoadingStateContext.Provider>
     </BlogContext.Provider>
   )
 }

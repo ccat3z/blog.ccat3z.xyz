eb0e0bad57111ff834bd72b9cda51073f450d27b
ccat3z <c0ldcat3z@gmail.com>
2020-08-31 21:34:52 +0800

    init root layout

--- app/src/components/Root.tsx
+++ app/src/components/Root.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import Header from './Header'
 
 type HeaderProps = {
   children: React.ReactNode
 }
 
-export default function Header(props: HeaderProps) {
-  return <div>
-    aa
-    {props.children}
+export default function Root(props: HeaderProps) {
+  return <div style={{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%'
+  }}>
+    <div style={{
+      maxWidth: '860px',
+      width: '95%'
+    }}>
+      <Header />
+      <div style={{
+        paddingBottom: '10px'
+      }}>
+        {props.children}
+      </div>
+    </div>
   </div>
 }

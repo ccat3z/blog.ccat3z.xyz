9b64485a0b8691d6128ea51a53ac832bd89290c3
ccat3z <c0ldcat3z@gmail.com>
2020-09-05 22:45:43 +0800

    header: refactor command

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,72 +1,98 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import { useBlogData, useBlogDataLoadingState } from '../BlogData'
 import { useHistory, useLocation } from 'react-router-dom'
+import debounce from 'lodash/debounce'
 import './Header.scss'
+
+function PureStringCommand(props: {text: string}) {
+  return <span className="command">{props.text}</span>
+}
+
+function PageFilterCommand(props: {page: number}) {
+  const { pagination } = useBlogData()
+
+  const { push: _goTo } = useHistory()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const goTo = useCallback(debounce((index: number) => {
+    if (pagination === undefined) return
+    if (index < 1) return
+    if (index > pagination.length) return
+    console.log('goto ' + pagination[index - 1].href)
+    _goTo(pagination[index - 1].href)
+  }, 500), [_goTo, pagination])
+
+  const [current, _setCurrent] = useState<number>(props.page)
+  const setCurrent = useCallback((p: number) => {
+    if (1 <= p && p <= (pagination || []).length)
+      _setCurrent(p)
+  }, [_setCurrent, pagination])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => goTo(current), [current])
+
+  let autocomplete = <></>
+  if (pagination !== undefined) {
+    const total = pagination.length
+    autocomplete = <span className="auto-complete">
+      &nbsp;of {total}
+      &nbsp;
+      <span className="page-btns">
+        (
+        <a
+          onClick={() => setCurrent(current - 1)}
+          className={`page-btn ${current === 1 ? "disabled" : ""}`}
+        >prev</a>,&nbsp;
+        <a
+          onClick={() => setCurrent(current + 1)}
+          className={`page-btn ${current === total ? "disabled" : ""}`}
+        >next</a>
+        )
+      </span>
+    </span>
+  }
+
+  return <span className="pager command">
+    page&nbsp;
+    <input
+      value={current}
+      onChange={v => setCurrent(parseInt(v.target.value))}
+      type="number"
+    />
+    {autocomplete}
+  </span>
+}
 
 type Location = {
   pathname: string
 }
 
 const virtualCommands = [
-  l => { if (l.pathname === '/') return 'screenfetch' },
-  l => { if (l.pathname.match(/^\/posts(|\/\d*)\/?$/)) return 'ls /posts'},
-  l => 'cat ' + l.pathname.replace(/\/$/, '').replace(/\.[^./]*/, '')
-] as ((locaction: Location) => string | undefined)[]
-
-function convertUrlToVirtualCommand(location: Location) {
-  let res = ''
-  for (let f of virtualCommands) {
-    res = f(location) || ''
-    if (res) return res
+  // /
+  l => {
+    if (l.pathname === '/') return <PureStringCommand text='screenfetch' />
+  },
+  // /posts /posts/:num
+  l => {
+    let m = l.pathname.match(/^\/posts(?:|\/(\d*))\/?$/)
+    if (m === null) return
+    let page = parseInt(m[1] || '1')
+    return <>
+      <PureStringCommand text='ls /posts' />
+      <PageFilterCommand page={page} />
+    </>
+  },
+  // /*
+  l => {
+    let c = 'cat ' + l.pathname.replace(/\/$/, '').replace(/\.[^./]*/, '')
+    return <PureStringCommand text={c} />
   }
-  return `cat ${location.pathname}`
-}
+] as ((locaction: Location) => React.ReactNode | undefined)[]
 
-function PageFilterCommand() {
-  const { pagination } = useBlogData()
-  const { push: _goTo } = useHistory()
-  const [current, setCurrent] = useState<number>()
-  const goTo = useCallback((index: number) => {
-    if (pagination === undefined) return
-    if (index < 1) return
-    if (index > pagination.length) return
-    _goTo(pagination[index - 1].href)
-  }, [_goTo, pagination])
-  useEffect(() => {
-    if (pagination) {
-      setCurrent(pagination.findIndex(p => p.current) + 1)
-    }
-  }, [pagination])
-
-  if (pagination === undefined || current === undefined) return <></>
-
-  const total = pagination.length
-
-  return <span className="pager">
-    &nbsp;| page&nbsp;
-    <input
-      value={current}
-      onChange={v => {
-        let p = parseInt(v.target.value)
-        if (1 <= p && p <= total) setCurrent(p)
-      }}
-      type="number"
-    />
-    &nbsp;of {total}
-    &nbsp;
-    <span className="page-btns">
-      (
-      <a
-        onClick={() => goTo(current - 1)}
-        className={`page-btn ${current === 1 ? "disabled" : "" }`}
-      >prev</a>,&nbsp;
-      <a
-        onClick={() => goTo(current + 1)}
-        className={`page-btn ${current === total ? "disabled" : "" }`}
-      >next</a>
-      )
-    </span>
-  </span>
+function convertUrlToVirtualCommand(location: Location): React.ReactNode {
+  let res: React.ReactNode = undefined
+  for (let f of virtualCommands) {
+    res = f(location)
+    if (res !== undefined) return res
+  }
 }
 
 export default function Header(props: {
@@ -86,10 +112,9 @@ export default function Header(props: {
           goTo('/')
         }
       }} />
-      <span>
-        {`${convertUrlToVirtualCommand({pathname})}`}
+      <span className="command-pipeline">
+        {convertUrlToVirtualCommand({pathname})}
       </span>
-      <PageFilterCommand />
       { state === 'pending' && <span
         style={{
           margin: '0 0.3em',

5c93cdcd5f2f80d8b70a1e4a9aaed40ee3b04540
ccat3z <c0ldcat3z@gmail.com>
2020-09-07 22:06:38 +0800

    wip page: remove redirect

--- app/src/pages/WIP.tsx
+++ app/src/pages/WIP.tsx
@@ -2,16 +2,7 @@ import React from 'react'
 import { registerPageType } from '../PageDispatcher'
 
 function WIPPage() {
-  const oldVersionUrl = `${window.location.origin}${window.location.pathname}?archive`
-  if (oldVersionUrl !== window.location.href) {
-    window.location.href = oldVersionUrl
-  }
-  return (
-    <div>
-      {'[WIP] '}
-      <a href={oldVersionUrl}>Redirecting to old version...</a>
-    </div>
-  )
+  return <div>WIP</div>
 }
 
 registerPageType({

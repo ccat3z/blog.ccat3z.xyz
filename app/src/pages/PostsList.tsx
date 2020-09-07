15d1e4baecb547bbca4922cf19819d79be8844ce
ccat3z <c0ldcat3z@gmail.com>
2020-09-07 18:35:25 +0800

    posts list: fix tag onClick action

--- app/src/pages/PostsList.tsx
+++ app/src/pages/PostsList.tsx
@@ -77,6 +77,7 @@ function PostsListPage() {
                   onClick={(e) => {
                     e.stopPropagation()
                     e.preventDefault()
+                    goTo(t.href)
                   }}
                 >{t.name}</a>
               </span>))}
